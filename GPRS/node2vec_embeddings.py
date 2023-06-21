85e3199bb72f3d5636fe8a01f916f93c2e3dbc09
Aed3r <xxgoffi22xx@gmail.com>
2023-06-21 15:07:14 +0200

    rdf encoding

--- GPRS/node2vec_embeddings.py
+++ GPRS/node2vec_embeddings.py
@@ -1,63 +1,65 @@
 import networkx as nx
-from node2vec import Node2Vec
+import pandas as pd
 from rdflib import Graph
-from tqdm import tqdm
-import time
+
 
 class Node2VecEmbeddings:
-    def __init__(self, graph_file, dimensions=128, walk_length=30, num_walks=200, workers=4):
-        self.graph_file = graph_file
+    def __init__(self, dimensions=128, walk_length=30, num_walks=200, workers=4):
         self.dimensions = dimensions
         self.walk_length = walk_length
         self.num_walks = num_walks
         self.workers = workers
         self.model = None
+        self.node_mapping = {}
+        self.edge_df = None
 
-    def load_graph(self):
+    def load_graph(self, graph_file):
         print("Loading RDF graph...")
-        start_time = time.time()
-
         graph = Graph()
-        graph.parse(self.graph_file, format='rdf')
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"RDF graph loaded in {elapsed_time:.2f} seconds")
-
+        graph.parse(graph_file, format='rdf')
         return graph
 
-    def generate_embeddings(self):
-        graph = self.load_graph()
+    def encode_nodes(self, graph):
+        self.node_mapping = {}
+        nodes = list(graph.all_nodes())
 
-        print("Creating NetworkX graph...")
-        start_time = time.time()
+        for i, node in enumerate(nodes):
+            self.node_mapping[node] = i
+
+        print("Nodes encoded as integers.")
+
+    def create_edge_dataframe(self, graph):
+        edge_data = []
+        for s, p, o in graph:
+            source = self.node_mapping.get(s)
+            target = self.node_mapping.get(o)
+            edge_type = p
+            edge_data.append((source, target, edge_type))
+
+        columns = ["Source", "Target", "Edge Type"]
+        self.edge_df = pd.DataFrame(edge_data, columns=columns)
+
+        print("Edge DataFrame created.")
+
+    def generate_embeddings(self, graph_file):
+        graph = self.load_graph(graph_file)
+
+        self.encode_nodes(graph)
+        self.create_edge_dataframe(graph)
 
         nx_graph = nx.Graph()
         for s, p, o in graph:
-            nx_graph.add_edge(s, o)
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"NetworkX graph created in {elapsed_time:.2f} seconds")
+            source = self.node_mapping.get(s)
+            target = self.node_mapping.get(o)
+            edge_type = p
+            nx_graph.add_edge(source, target, edge_type=edge_type)
 
         print("Initializing node2vec model...")
-        start_time = time.time()
-
         node2vec = Node2Vec(nx_graph, dimensions=self.dimensions, walk_length=self.walk_length,
                             num_walks=self.num_walks, workers=self.workers)
 
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Node2Vec model initialized in {elapsed_time:.2f} seconds")
-
         print("Generating node2vec embeddings...")
-        start_time = time.time()
-
         self.model = node2vec.fit(window=10, min_count=1, batch_words=4)
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Node2Vec embeddings generated in {elapsed_time:.2f} seconds")
 
         print("Embeddings generated for all nodes!")
 
@@ -66,47 +68,54 @@ class Node2VecEmbeddings:
         nx_graph.add_nodes_from(new_nodes)
 
         print("Updating node2vec model with new nodes...")
-        start_time = time.time()
-
         self.model = self.model.fit(window=10, min_count=1, batch_words=4)
 
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Node2Vec model updated with new nodes in {elapsed_time:.2f} seconds")
-
     def get_embeddings(self, nodes):
-        embeddings = {}
-        progress_bar = tqdm(total=len(nodes), desc="Retrieving Embeddings")
-
-        for node in nodes:
-            start_time = time.time()
-            embeddings[node] = self.model.wv[node]
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            progress_bar.set_postfix({"Elapsed Time": f"{elapsed_time:.2f} s"})
-            progress_bar.update(1)
-
-        progress_bar.close()
-        print("Embeddings retrieved for all nodes!")
-
+        embeddings = {node: self.model.wv[node] for node in nodes}
         return embeddings
 
 
+# Function to convert RDF graph to edge DataFrame
+def rdf_graph_to_edge_dataframe(graph_file):
+    graph = Graph()
+    graph.parse(graph_file, format='rdf')
+
+    edge_data = []
+    for s, p, o in graph:
+        edge_data.append((s, o, p))
+
+    columns = ["Source Node", "Target Node", "Edge Type"]
+    edge_df = pd.DataFrame(edge_data, columns=columns)
+
+    return edge_df
+
+
 # Example usage:
-embeddings_generator = Node2VecEmbeddings('your_graph.rdf', dimensions=128, walk_length=30, num_walks=200, workers=4)
+embeddings_generator = Node2VecEmbeddings(dimensions=128, walk_length=30, num_walks=200, workers=4)
 
-# Generate embeddings
-embeddings_generator.generate_embeddings()
+# Generate embeddings and create edge DataFrame
+embeddings_generator.generate_embeddings('your_graph.rdf')
 
-# New nodes to be added
+# Get the encoded node mapping and edge DataFrame
+node_mapping = embeddings_generator.node_mapping
+edge_df = embeddings_generator.edge_df
+
+# Add new nodes and update the embeddings
 new_nodes = ['new_node1', 'new_node2', 'new_node3']
-
-# Add the new nodes and update the embeddings
 embeddings_generator.add_new_nodes(new_nodes)
 
 # Get the updated embeddings for the new nodes
 new_node_embeddings = embeddings_generator.get_embeddings(new_nodes)
 
-# Print the embeddings for the new nodes
-for node, embedding in new_node_embeddings.items():
-    print(f"Embedding for '{node}': {embedding}")
+# Convert RDF graph to edge DataFrame
+rdf_graph_file = 'your_graph.rdf'
+edge_df = rdf_graph_to_edge_dataframe(rdf_graph_file)
+
+# Print the encoded node mapping
+print("Node Mapping:")
+for node, encoding in node_mapping.items():
+    print(f"{node} -> {encoding}")
+
+# Print the edge DataFrame
+print("Edge DataFrame:")
+print(edge_df)
